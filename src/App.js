fde6ef04e5629e0a22d512ad16d4fa358c1db526
= <zrsmead@gmail.com>
2018-06-27 02:22:35 -0700

    config for firebase

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import firebaseConfig from './firebaseSetup.js';
+import reducers from './reducers';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>
+            Hello!
+          </Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
