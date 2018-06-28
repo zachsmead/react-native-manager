d708cc573794e1541ae15bb7d4ed636ad56aa6ed
= <zrsmead@gmail.com>
2018-06-27 20:39:05 -0700

    login helper methods and more dispatch

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,21 +1,26 @@
 import {
   EMAIL_CHANGED,
-  PASSWORD_CHANGED
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL
 } from '../actions/types';
 
 const INITIAL_STATE = {
   email: '',
-  password: ''
+  password: '',
+  user: null
 };
 
 export default AuthReducer = (state = INITIAL_STATE, action) => {
   console.log(action);
-  
+
   switch (action.type) {
     case EMAIL_CHANGED:
       return { ...state, email: action.payload};
     case PASSWORD_CHANGED:
-      return { ...state, PASSWORD: action.payload};
+      return { ...state, password: action.payload};
+    case LOGIN_USER_SUCCESS:
+      return { ...state, user: action.payload};
     default:
       return state;
   }
