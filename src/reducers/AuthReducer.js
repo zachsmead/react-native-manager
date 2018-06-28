a00f129a9a147acb4c9d49bc2c5c2603d4d1c958
= <zrsmead@gmail.com>
2018-06-27 18:40:04 -0700

    asynchronous actions, firebase login and redux thunk

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,11 +1,19 @@
-import { EMAIL_CHANGED } from '../actions/types';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED
+} from '../actions/types';
 
-const INITIAL_STATE = { email: '' };
+const INITIAL_STATE = {
+  email: '',
+  password: ''
+};
 
 export default AuthReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMAIL_CHANGED:
-      return action.payload;
+      return { ...state, email: action.payload};
+    case PASSWORD_CHANGED:
+      return { ...state, PASSWORD: action.payload};
     default:
       return state;
   }
