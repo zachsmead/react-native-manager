d439e19676c03b00ce0b298019a1055ca5679cca
= <zrsmead@gmail.com>
2018-06-27 21:19:19 -0700

    set login form to clear fields on login sucess

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -2,14 +2,16 @@ import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
   LOGIN_USER_SUCCESS,
-  LOGIN_USER_FAIL
+  LOGIN_USER_FAIL,
+  LOGIN_USER
 } from '../actions/types';
 
 const INITIAL_STATE = {
   email: '',
   password: '',
   user: null,
-  error: ''
+  error: '',
+  loading: false
 };
 
 export default AuthReducer = (state = INITIAL_STATE, action) => {
@@ -17,13 +19,20 @@ export default AuthReducer = (state = INITIAL_STATE, action) => {
 
   switch (action.type) {
     case EMAIL_CHANGED:
-      return { ...state, email: action.payload};
+      return { ...state, email: action.payload };
     case PASSWORD_CHANGED:
-      return { ...state, password: action.payload};
+      return { ...state, password: action.payload };
+    case LOGIN_USER:
+      return { ...state, loading: true, error: '' }
     case LOGIN_USER_SUCCESS:
-      return { ...state, user: action.payload, error: ''};
+      return { ...state, ...INITIAL_STATE, user: action.payload, };
     case LOGIN_USER_FAIL:
-      return { ...state, error: 'Authentication Failed.', password: ''};
+      return {
+        ...state,
+        error: 'Authentication Failed.',
+        password: '',
+        loading: false
+      };
     default:
       return state;
   }
